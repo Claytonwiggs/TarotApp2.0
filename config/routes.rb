662bfc840e9f77f8b08ff0786aba4a91d5160ab0
claytonwiggs <claytonwiggs@gmail.com>
2021-10-02 10:52:31 -0400

    Cards Crud Backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/cards" => "cards#index"
+  post "/cards" => "cards#create"
+  get "/cards/:id" => "cards#show"
+  patch "/cards/:id" => "cards#update"
+  delete "/cards/:id" => "cards#destroy"
 end
