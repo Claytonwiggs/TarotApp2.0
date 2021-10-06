bc2adf59cc2ca8c40706db78c66d5c83db185906
claytonwiggs <claytonwiggs@gmail.com>
2021-10-06 16:02:36 -0400

    Added User Models Controller and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get "/cards/:id" => "cards#show"
   patch "/cards/:id" => "cards#update"
   delete "/cards/:id" => "cards#destroy"
+
+  get "/users" => "users#index"
+  post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
 end
