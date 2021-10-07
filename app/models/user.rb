cfc4e6b59f608da87ea95cbf14c5a9cdbb3298da
claytonwiggs <claytonwiggs@gmail.com>
2021-10-07 09:48:51 -0400

    added template controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ApplicationRecord
+  def index
+    users = User.all
+    render json: users.as_json
+  end
+
+  def create
+    user = User.new(
+      username: params[:name]
+    )
+    user.save
+    render json: user.as_json
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user.as_json
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.name = params[:name] || user.name
+    user.width = params[:width] || user.width
+    user.height = params[:height] || user.height
+    user.save
+    render json: user.as_json
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: { message: "User successfully destroyed." }
+  end
 end
